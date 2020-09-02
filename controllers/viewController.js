18d1f43d7c616c3e5320cc9b5dbfc1b243d43455
Prajjwal Arora <aroraprajjwal@gmail.com>
2020-09-02 22:32:00 +0530

    Bug Fix

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -30,11 +30,14 @@ exports.getTour = catchAsync(async (req, res, next) => {
   if (!tour) {
     return next(new AppError('There is no tour with that name.', 404));
   }
-  const bookings = await Booking.find({
-    user: res.locals.user.id,
-    tour: tour.id
-  });
-  res.locals.isBookedTour = bookings.length > 0;
+
+  if (res.locals.user) {
+    const bookings = await Booking.find({
+      user: res.locals.user.id,
+      tour: tour.id
+    });
+    res.locals.isBookedTour = bookings.length > 0;
+  }
 
   res
     .status(200)
